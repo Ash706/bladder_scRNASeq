498aaba122e4b73372cb4e51537e1caa9da93c23
Ash706 <ashfaq.ali@nbis.se>
2022-01-14 14:13:31 +0100

    bash script for loom files from bam

---
+++ cell_ranger.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#SBATCH -A snic2021-22-481
+#SBATCH -p node -N 1 -C mem256GB
+##SBATCH -n 20
+#SBATCH -t 24:00:00
+#SBATCH -J create_bam
+#SBATCH -o process_%j.out
+#SBATCH -e process_%j.err
+#SBATCH --mail-user=ashfaq.ali@immun.lth.se
+#SBATCH --mail-type=END
+
+cd /proj/snic2021-23-431/private/Dvid/
+
+module load bioinfo-tools
+module load cellranger/3.0.1
+
+
+
+cellranger count --id='TC2_new' --fastqs='./dvid/TC2_fastq/'  --sample='sample_2' --transcriptome='$CELLRANGER_DATA/'refdata-cellranger-GRCh38-3.0.0 --localcores=20 --localmem=256
+
